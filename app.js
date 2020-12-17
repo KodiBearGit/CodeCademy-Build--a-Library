10fd84f4f7bb61424fcab37bc9385f92d2ef5919
Chris <chris.tisserat@gmail.com>
2020-12-17 13:06:36 -0800

    13of25 complete sub-class Book

--- app.js
+++ app.js
@@ -37,7 +37,15 @@ class Media {
 }
 
 class Book extends Media {
-  constructor(title, ) {
+  constructor(title, author, pages) {
     super(title);
+    this._author = author;
+    this._pages = pages;
+  }
+  get author() {
+    return this._author;
+  }
+  get pages() {
+    return this._pages;
   }
  }
